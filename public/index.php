dc7fc30adb2dc026644447d13b00eae6dfc86a4b
MMXXII <perfilev621@gmail.com>
2025-03-15 00:26:19 +0800

    Добвлены контроллер для 404 страницы, сделана структура роутера плоской

--- public/index.php
+++ public/index.php
@@ -1,23 +1,25 @@
-<?php 
+<?php
 require_once "../vendor/autoload.php";
-require_once "../controllers/MainController.php"; // добавим в самом верху ссылку на наш контроллер
-require_once "../controllers/WindowsController.php"; 
+require_once "../controllers/MainController.php"; 
+require_once "../controllers/WindowsController.php";
 require_once "../controllers/WindowsImageController.php";
 require_once "../controllers/WindowsInfoController.php";
 require_once "../controllers/LinuxController.php";
 require_once "../controllers/LinuxImageController.php";
 require_once "../controllers/LinuxInfoController.php";
+require_once "../controllers/Controller404.php"; 
 
-$loader = new \Twig\Loader\FilesystemLoader('C:\Users\perfi\Desktop\study\4\Web programming\Backend\back1_1\views');
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../views');
 $twig = new \Twig\Environment($loader);
 
 $url = $_SERVER["REQUEST_URI"];
 
 $title = "";
 $template = "";
-$context = []; 
+$context = [];
 
-$controller = null; // создаем переменную под контроллер
+$controller = new Controller404($twig); // создаем переменную под контроллер
 $menu = [
     [
         "title" => "Главная",
@@ -35,30 +37,25 @@ $menu = [
 
 if ($url == "/") {
     $controller = new MainController($twig);
+} elseif (preg_match("#/windows/image#", $url)) {
+    $controller = new WindowsImageController($twig);
+} elseif (preg_match("#/windows/info#", $url)) {
+    $controller = new WindowsInfoController($twig);
 } elseif (preg_match("#^/windows#", $url)) {
     $controller = new windowsController($twig);
-
-    if (preg_match("#/windows/image#", $url)) {
-        $controller = new WindowsImageController($twig);
-    } elseif (preg_match("#/windows/info#", $url)) {
-        $controller = new WindowsInfoController($twig);
-    } else {
-        $template = "__object.twig";
-    }
-
-
-
+} else {
+    $template = "__object.twig";
+}
+if (preg_match("#/linux/image#", $url)) {
+    $controller = new LinuxImageController($twig);
+} elseif (preg_match("#/linux/info#", $url)) {
+    $controller = new LinuxInfoController($twig);
 } elseif (preg_match("#^/linux#", $url)) {
     $controller = new LinuxController($twig);
-
-    if (preg_match("#/linux/image#", $url)) {
-        $controller = new LinuxImageController($twig);
-    } elseif (preg_match("#/linux/info#", $url)) {
-        $controller = new LinuxInfoController($twig);
-    } else {
-        $template = "__object.twig";
-    }
+} else {
+    $template = "__object.twig";
 }
+
 
 // $context['title'] = $title;
 $context['menu'] = $menu;
