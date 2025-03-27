d4f34295685226d93c1689b7f4082ba0132da3eb
MMXXII <perfilev621@gmail.com>
2025-03-27 15:56:14 +0800

    Подключена бд, данные берутся с нее, изменена страница main, подключены стили  CRUD 1

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -3,7 +3,7 @@ require_once "TwigBaseController.php"; // импортим TwigBaseController
 
 class MainController extends TwigBaseController {
     public $template = "main.twig";
-    public $title = "Главная";
+
 
     public function getContext(): array
     {
